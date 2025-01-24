4761e8f6da5582b4efd0f4973f5086f534ce336c
daneeelo <daneelo.code@proton.me>
2025-01-24 22:01:17 +0100

    primeVue, pinia, vite.config setup

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
