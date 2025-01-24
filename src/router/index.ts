8f4a81f73b4ebb72f24a241342a919cb08717032
daneeelo <daneelo.code@proton.me>
2025-01-24 22:07:27 +0100

    remove template and unused components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
+import Home from '@/views/home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +7,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/second-page',
+      name: 'second-page',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/second-page.vue'),
     },
   ],
 })
